872e78bbb591a2753914688df70e10136e470891
Stephen Higgins <stephen.higgins1995@gmail.com>
2025-08-27 17:31:10 +0100

    Refactor schema imports for better modularization.
    Replaced single aggregated import with individual imports for entities and relations. This improves clarity and maintainability by explicitly defining each schema element in the `schema` object.

--- libs/data-access-layer/src/orm/schema.ts
+++ libs/data-access-layer/src/orm/schema.ts
@@ -1,5 +1,27 @@
-import * as tablesAndRelations from './schemas';
+import * as abilityEntity from './schemas/abilities/ability.entity';
+import * as abilityRelations from './schemas/abilities/ability.relations';
+import * as pokemonAbilityEntity from './schemas/pokemon-abilities/pokemon-ability.entity';
+import * as pokemonAbilityRelations from './schemas/pokemon-abilities/pokemon-ability.relations';
+import * as pokemonTypeEntity from './schemas/pokemon-types/pokemon-type.entity';
+import * as pokemonTypeRelations from './schemas/pokemon-types/pokemon-type.relations';
+import * as pokemonEntity from './schemas/pokemons/pokemon.entity';
+import * as pokemonRelations from './schemas/pokemons/pokemon.relations';
+import * as typeEntity from './schemas/types/type.entity';
+import * as typeRelations from './schemas/types/type.relations';
+import * as userEntity from './schemas/users/user.entity';
+import * as userRelations from './schemas/users/user.relations';
 
 export const schema = {
-  ...tablesAndRelations,
+  abilityEntity,
+  abilityRelations,
+  pokemonAbilityEntity,
+  pokemonAbilityRelations,
+  pokemonTypeEntity,
+  pokemonTypeRelations,
+  pokemonEntity,
+  pokemonRelations,
+  typeEntity,
+  typeRelations,
+  userEntity,
+  userRelations,
 };
