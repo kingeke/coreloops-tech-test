a6fba7422ceb453ef593ca8f9167ca34485a6587
Stephen Higgins <stephen.higgins1995@gmail.com>
2025-08-27 17:42:16 +0100

    Refactor schema imports for better modularization.
    Replaced single aggregated import with individual imports for entities and relations. This improves clarity and maintainability by explicitly defining each schema element in the `schema` object.

--- libs/data-access-layer/src/orm/schema.ts
+++ libs/data-access-layer/src/orm/schema.ts
@@ -1,27 +1,27 @@
-import * as abilityEntity from './schemas/abilities/ability.entity';
-import * as abilityRelations from './schemas/abilities/ability.relations';
-import * as pokemonAbilityEntity from './schemas/pokemon-abilities/pokemon-ability.entity';
-import * as pokemonAbilityRelations from './schemas/pokemon-abilities/pokemon-ability.relations';
-import * as pokemonTypeEntity from './schemas/pokemon-types/pokemon-type.entity';
-import * as pokemonTypeRelations from './schemas/pokemon-types/pokemon-type.relations';
-import * as pokemonEntity from './schemas/pokemons/pokemon.entity';
-import * as pokemonRelations from './schemas/pokemons/pokemon.relations';
-import * as typeEntity from './schemas/types/type.entity';
-import * as typeRelations from './schemas/types/type.relations';
-import * as userEntity from './schemas/users/user.entity';
-import * as userRelations from './schemas/users/user.relations';
+import { abilityEntity } from '@coreloops-orm/schemas/abilities/ability.entity';
+import * as abilityRelations from '@coreloops-orm/schemas/abilities/ability.relations';
+import { pokemonAbilityEntity } from '@coreloops-orm/schemas/pokemon-abilities/pokemon-ability.entity';
+import * as pokemonAbilityRelations from '@coreloops-orm/schemas/pokemon-abilities/pokemon-ability.relations';
+import { pokemonTypeEntity } from '@coreloops-orm/schemas/pokemon-types/pokemon-type.entity';
+import * as pokemonTypeRelations from '@coreloops-orm/schemas/pokemon-types/pokemon-type.relations';
+import { pokemonEntity } from '@coreloops-orm/schemas/pokemons/pokemon.entity';
+import * as pokemonRelations from '@coreloops-orm/schemas/pokemons/pokemon.relations';
+import { typeEntity } from '@coreloops-orm/schemas/types/type.entity';
+import * as typeRelations from '@coreloops-orm/schemas/types/type.relations';
+import { userEntity } from '@coreloops-orm/schemas/users/user.entity';
+import * as userRelations from '@coreloops-orm/schemas/users/user.relations';
 
 export const schema = {
-  ...abilityEntity,
-  ...abilityRelations,
-  ...pokemonAbilityEntity,
-  ...pokemonAbilityRelations,
-  ...pokemonTypeEntity,
-  ...pokemonTypeRelations,
-  ...pokemonEntity,
-  ...pokemonRelations,
-  ...typeEntity,
-  ...typeRelations,
-  ...userEntity,
-  ...userRelations,
-};
+  abilityEntity,
+  abilityRelations,
+  pokemonAbilityEntity,
+  pokemonAbilityRelations,
+  pokemonTypeEntity,
+  pokemonTypeRelations,
+  pokemonEntity,
+  pokemonRelations,
+  typeEntity,
+  typeRelations,
+  userEntity,
+  userRelations,
+} as const;
